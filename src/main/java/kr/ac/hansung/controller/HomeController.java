760128ac6d50f9c3843ba2b086e70332e0dffaa7
stelladream2 <nykim@hansung.ac.kr>
2025-03-01 12:11:11 +0900

    update pom.xml

--- src/main/java/kr/ac/hansung/controller/HomeController.java
+++ src/main/java/kr/ac/hansung/controller/HomeController.java
@@ -1,12 +1,12 @@
 package kr.ac.hansung.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
